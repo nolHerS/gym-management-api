26bbc69253af4f45fd78d360e55a802b7e5cdfb2
nolHerS <lonamish@gmail.com>
2026-07-27 23:43:31 +0200

    chore: initialize Spring Boot project

---
+++ src/main/java/com/imanol/gym/GymManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.imanol.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imanol/gym/GymManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imanol.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
